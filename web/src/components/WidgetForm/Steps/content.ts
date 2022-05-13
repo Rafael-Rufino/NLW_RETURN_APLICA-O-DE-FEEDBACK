81cc2e7cc2fb70302b1018ffa5436f40c078da5e
RafaelRufino <rafaelrufino.52@gmail.com>
2022-05-13 16:18:33 -0300

    compoennt de widget do web

---
+++ web/src/components/WidgetForm/Steps/content.ts
@@ -0,0 +1,28 @@
+import bugImageUrl from "../../../assets/bug.svg";
+import ideaImageUrl from "../../../assets/idea.svg";
+import thoughtImageUrl from "../../../assets/thought.svg";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de um lampada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de um balão de um pensamento",
+    },
+  },
+};
+export type FeedbackType = keyof typeof feedbackTypes;
